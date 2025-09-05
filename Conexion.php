f77626181557b0642866b038450227b0846c5f0f
FrancoVega852 <francoagustin352@gmail.com>
2025-09-05 11:40:57 -0300

    Update Conexion.php
    Nueva conexion con la base de datos

--- Conexion.php
+++ Conexion.php
@@ -1,13 +1,9 @@
 <?php
 class Conexion {
-    private $host = "localhost";
-    private $usuario = "root";
-    private $password = "";
-    private $bd = "farmacia";
     public $conexion;
 
     public function __construct() {
-        $this->conexion = new mysqli($this->host, $this->usuario, $this->password, $this->bd);
+        $this->conexion = new mysqli("localhost", "root", "", "farmacia"); 
         if ($this->conexion->connect_error) {
             die("Error de conexión: " . $this->conexion->connect_error);
         }
